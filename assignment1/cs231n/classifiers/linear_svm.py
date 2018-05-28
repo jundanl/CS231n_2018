bbba83d7e0902435d5742b22e71c388bb1b39439
Unknown <598991258@qq.com>
2018-05-28 16:49:46 +0800

    finish svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,13 +34,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i, :]
+        dW[:, y[i]] -= X[i, :]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += reg*2*W
 
   #############################################################################
   # TODO:                                                                     #
@@ -61,6 +65,8 @@ def svm_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as svm_loss_naive.
   """
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
@@ -69,7 +75,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  correct_class_scores = scores[range(num_train), list(y)].reshape(-1,1)
+  margins = np.maximum(0, scores - correct_class_scores + 1)
+  margins[range(num_train), list(y)] = 0
+  loss = np.sum(margins) / num_train + reg * np.sum(W * W)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +94,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  coeffients = np.zeros(margins.shape)
+  coeffients[margins > 0] = 1
+  coeffients[range(num_train), list(y)] = -np.sum(coeffients,axis=1)
+  dW = (X.T).dot(coeffients) / num_train
+  dW += reg*2*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
